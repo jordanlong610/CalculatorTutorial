7acb41f87e3382eb50074b8d3cd26de93416616b
Jordan Long <jordan@promogroupllc.com>
2018-03-17 13:49:35 -0400

    Intial Commit

---
+++ CalculatorTutorial/Calculator.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "Calculator.h"
+
+
+
+double Calculator::Calculate(double x, char oper, double y)
+{
+	switch (oper)
+	{
+	case '+':
+		return x + y;
+	case '-':
+		return x - y;
+	case '*':
+		return x * y;
+	case '/':
+		return x / y;
+	default:
+		return 0;
+	}
+
+}
